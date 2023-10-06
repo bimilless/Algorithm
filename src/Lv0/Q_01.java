1bf577bf2a484003a7078fe2f6740f78cc8aac7c
bimilless <bimils7856@gmail.com>
2023-10-06 17:09:39 +0900

    refactor: 파일 이름 변경

--- src/Lv0/Q_01.java
+++ src/Lv0/Q_01.java
@@ -2,7 +2,7 @@ package Lv0;
 
 import java.util.Scanner;
 
-public class StringPrint {
+public class Q_01 {
     public static void main(String[] args) {
         // 문자열 str이 주어질 때, str을 출력하는 코드를 작성해 보세요.
 
--- src/Lv0/Q_02.java
+++ src/Lv0/Q_02.java
@@ -2,7 +2,7 @@ package Lv0;
 
 import java.util.Scanner;
 
-public class ABprint {
+public class Q_02 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
