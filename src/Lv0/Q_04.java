922d671253d0fdaf9904bf72bb5991235f9bb5f9
bimilless <bimils7856@gmail.com>
2023-10-10 14:57:38 +0900

    Q_04 문제 및 조건 추가

--- src/Lv0/Q_04.java
+++ src/Lv0/Q_04.java
@@ -4,6 +4,15 @@ import java.util.Scanner;
 
 public class Q_04 {
     public static void main(String[] args) {
+        // 문제 설명
+        // 영어 알파벳으로 이루어진 문자열 str이 주어집니다. 각 알파벳을 대문자는 소문자로 소문자는 대문자로 변환해서 출력하는 코드를 작성해 보세요.
+
+        // 입력 #1
+        // aBcDeFg
+
+        // 출력 #1
+        // AbCdEfG
+
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
         String result = "";
