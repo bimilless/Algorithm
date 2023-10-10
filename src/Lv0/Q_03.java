3e256b2a5b3c9a9f05c5634e5d8db7f0a723cab9
bimilless <bimils7856@gmail.com>
2023-10-10 14:56:45 +0900

    Q_03 문제 및 조건 추가

--- src/Lv0/Q_03.java
+++ src/Lv0/Q_03.java
@@ -4,6 +4,17 @@ import java.util.Scanner;
 
 public class Q_03 {
     public static void main(String[] args) {
+        // 문제 설명
+        // 문자열 str과 정수 n이 주어집니다.
+        // str이 n번 반복된 문자열을 만들어 출력하는 코드를 작성해 보세요.
+
+        // 입력 #1
+        // string 5
+
+        // 출력 #1
+        //stringstringstringstringstring
+
+
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
         int n = sc.nextInt();
