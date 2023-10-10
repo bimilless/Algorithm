c822ab14e6df36dbac9ec55871bc7c0ea93e4088
bimilless <bimils7856@gmail.com>
2023-10-10 15:00:27 +0900

    Q_06 문제 및 조건 추가

--- src/Lv0/Q_06.java
+++ src/Lv0/Q_06.java
@@ -4,6 +4,16 @@ import java.util.Scanner;
 
 public class Q_06 {
     public static void main(String[] args) {
+        // 문제 설명
+        // 두 정수 a, b가 주어질 때 다음과 같은 형태의 계산식을 출력하는 코드를 작성해 보세요.
+        // a + b = c
+
+        // 입력 #1
+        // 4 5
+
+        // 출력 #1
+        // 4 + 5 = 9
+
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
