4323ae8c5b00be02aec56443c747a9c2d7c84cbf
bimilless <bimils7856@gmail.com>
2023-10-19 17:47:10 +0900

    Q_16 수정

--- src/Lv0/Q_16.java
+++ src/Lv0/Q_16.java
@@ -36,6 +36,11 @@ public class Q_16 {
     class Solution {
         public int solution(int num, int n) {
             int answer = 0;
+            if (num % n == 0) {
+                answer = 1;
+            } else {
+                answer = 0;
+            }
             return answer;
         }
     }
