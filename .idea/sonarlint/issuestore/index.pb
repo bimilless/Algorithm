c250fe9f94453578849b41267fea15118a8d796e
bimilless <bimils7856@gmail.com>
2023-10-21 21:08:56 +0900

    flag에 따라 다른 값 반환하기

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,7 @@ A
 src/Lv0/Q_02.java,2\d\2d4a542f18bc14f75a8e092346b2157cfd7eb7bb
 A
 src/Lv0/Q_16.java,7\e\7e328622d53033e01769bb238f998684af6e3f74
+A
+src/Lv0/Q_18.java,0\b\0b0b269b62aad52e47b88d3caa24d001e93a97e3
+A
+src/Lv0/Q_19.java,6\5\65ff6832e77dfbbcbb6ca96840b770a78cb6d919
---
+++ src/Lv0/Q_20.java
@@ -0,0 +1,47 @@
+package Lv0;
+
+public class Q_20 {
+    public static void main(String[] args) {
+        /*
+        문제 설명
+
+        두 정수 a, b와 boolean 변수 flag가 매개변수로 주어질 때,
+        flag가 true면 a + b를 false면 a - b를 return 하는 solution 함수를 작성해 주세요.
+
+         */
+
+        /*
+        입출력 예
+        a	b	flag	result
+        -4	7	true	3
+        -4	7	false	-11
+
+        입출력 예
+        입출력 예 #1
+
+        예제 1번에서 flag가 true이므로 a + b = (-4) + 7 = 3을 return 합니다.
+
+        입출력 예 #2
+
+        예제 2번에서 flag가 false이므로 a - b = (-4) - 7 = -11을 return 합니다.
+         */
+
+        int a = -4;
+        int b = 7;
+        boolean flag = false;
+
+        if(flag == true) {
+            System.out.println(a+b);
+        } else {
+            System.out.println(Math.abs(a-b));
+        }
+
+    }
+
+    class Solution {
+        public int solution(int a, int b, boolean flag) {
+            int answer = 0;
+            return flag ? a+b : a-b;
+        }
+    }
+}
